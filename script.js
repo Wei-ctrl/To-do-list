db87646c9d3aa214ae022756c36f1bf52f11cb8f
unknown <weiphyoeaung.2021@gmail.com>
2025-08-11 10:08:46 +0630

    fix minor issuse, task update UI WIP

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const taskInput = document.getElementById("add-task");
 const addButton = document.getElementById("add-btn");
 
-let taskList = JSON.parse(localStorage.getItem('task')) || [];
+let taskList = JSON.parse(localStorage.getItem("task")) || [];
 let id = taskList.length > 0 ? taskList[taskList.length - 1].id : 0;
 
 addButton.addEventListener("click", () => {
@@ -11,26 +11,40 @@ addButton.addEventListener("click", () => {
     taskList.push(task);
     localStorage.setItem("task", JSON.stringify(taskList));
     taskInput.value = "";
-
-    const taskElement = document.createElement('div')
-    const taskContainer = document.getElementById('task-container')
-    taskContainer.appendChild(taskElement)
-    const taskName = document.createElement('p')
-    taskName.innerText = JSON.parse(localStorage.getItem('task'))[id-1].toDo;
-    //refreshes dont maintain the UI
-    const doneBtn = document.createElement('button')
-    doneBtn.innerText = 'Done'
-    taskElement.appendChild(taskName)
-    taskElement.appendChild(doneBtn)
-  }
+loadTasks() 
+ }
 });
 
-function loadTasks(){
-    const taskContainer = document.getElementById('task-container')
-    if(taskList.length < 1){
-        const noTask = document.createElement('h3')
-        noTask.innerText = 'No Task'
-        taskContainer.appendChild(noTask)
+function createTasks() {  
+  const taskElement = document.createElement("div");
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.appendChild(taskElement);
+  const taskName = document.createElement("p");
+  taskName.innerText = JSON.parse(localStorage.getItem("task"))[id - 1].toDo;
+  //refreshes dont maintain the UI
+  const doneBtn = document.createElement("button");
+  doneBtn.innerText = "Done";
+  taskElement.appendChild(taskName);
+  taskElement.appendChild(doneBtn);
+}
+
+function loadTasks() {
+    const taskContainer = document.getElementById('task-container');
+    const noTask = taskContainer.querySelector('.no-task'); // find existing
+
+    if (taskList.length < 1) {
+        if (!noTask) {
+            const newNoTask = document.createElement('h3');
+            newNoTask.classList.add('no-task');
+            newNoTask.innerText = 'No Task';
+            taskContainer.appendChild(newNoTask);
+        }
+    } else {
+      createTasks();
+        if (noTask) {
+            taskContainer.removeChild(noTask);
+        }
     }
 }
 
+loadTasks();
